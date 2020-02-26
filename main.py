0cad97ed7e6366e0485a367dca7f90e4abd1f0ea
quenting44 <gallouedec.quentin@gmail.com>
2020-02-26 17:20:50 +0100

    update model

---
+++ main.py
@@ -0,0 +1,305 @@
+# coding:utf-8
+
+"""Challenge data : predicting Paris Brigade Fire Response"""
+
+import torch  # basic tools
+
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn  # Module classe
+import torch.nn.functional as F  # functionnals like relu...
+import torch.optim as optim  # L'optimizer
+from torch.distributions import transforms
+from torch.optim.lr_scheduler import StepLR
+
+import numpy as np
+import pickle
+import sklearn.metrics
+import time
+import math
+import csv
+
+import logging
+logging.basicConfig(filename='logs/response_time.log', filemode='a',
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+# Select device
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+logging.info(' '.join(("Device used :", str(device))))
+
+# Path to save the model
+PATH = './nets/net.pth'
+
+
+def tensor_to_score(cpu_y, cpu_y_pred):
+    return(sklearn.metrics.r2_score(
+        cpu_y.detach().numpy(),
+        cpu_y_pred.detach().numpy()))
+
+
+def score_on_testset(net, validation_loader):
+    test_scores = []
+
+    for cpu_x, cpu_y in validation_loader:
+        logging.debug("data loaded")
+
+        # Set net mode to eval
+        net.eval()
+        logging.debug("network in train mode")
+
+        # send inputs and output on gpu
+        gpu_x, gpu_y = cpu_x.to(device), cpu_y.to(device)
+        logging.debug("x and y sent on gpu")
+
+        # compute output
+        gpu_y_pred = net(gpu_x)
+        logging.debug("output computed")
+
+        # compute score
+        cpu_y_pred = gpu_y_pred.cpu()
+        test_score = tensor_to_score(cpu_y, cpu_y_pred)
+        logging.debug("score computed")
+
+        # store loss
+        test_scores.append(test_score)
+
+    return np.mean(test_scores)
+
+
+def loss_on_testset(net, validation_loader, criterion):
+    """ Test the model (on validation_set), return the mean loss"""
+
+    test_losses = []
+
+    for cpu_x, cpu_y in validation_loader:
+        logging.debug("data loaded")
+
+        # Set net mode to eval
+        net.eval()
+        logging.debug("network in train mode")
+
+        # send inputs and output on gpu
+        gpu_x, gpu_y = cpu_x.to(device), cpu_y.to(device)
+        logging.debug("x and y sent on gpu")
+
+        # compute output
+        gpu_y_pred = net(gpu_x)
+        logging.debug("output computed")
+
+        # compute loss
+        loss = criterion(gpu_y_pred, gpu_y)
+        logging.debug("loss computed")
+
+        # store loss
+        test_losses.append(loss.item())
+
+    return np.mean(test_losses)
+
+
+class ResponseTimeDataset(Dataset):
+    """Response Time dataset."""
+
+    def __init__(self, train, slice_idx=0, nb_slices=50):
+        super(ResponseTimeDataset, self).__init__()
+
+        # load the np arrays
+        x_train = pickle.load(open('data/x_train.pickle', 'rb'))
+        x_test = pickle.load(open('data/x_test.pickle', 'rb'))
+        y = pickle.load(open('data/y_train.pickle', 'rb'))
+
+        x = np.concatenate((x_train, x_test))
+
+        # choose witch part of data to use
+        nb_data = x_train.shape[0]
+        validation_set_start_idx = int(slice_idx/nb_slices*nb_data)
+        validation_set_stop_idx = int((slice_idx+1)/nb_slices*nb_data)
+
+        if not train:  # if validation set
+            self.inputs = x_train[validation_set_start_idx:validation_set_stop_idx]
+            self.outputs = y[validation_set_start_idx:validation_set_stop_idx]
+
+        else:
+            # take the right slice
+            self.inputs = np.concatenate(
+                (x_train[:validation_set_start_idx], x_train[validation_set_stop_idx:]))
+            self.outputs = np.concatenate(
+                (y[:validation_set_start_idx], y[validation_set_stop_idx:]))
+
+        # mean reduce x
+        self._mean_reduce_input(x)
+
+    def _mean_reduce_input(self, x):
+        mean = x.mean(axis=0)
+        std = np.std(x, axis=0)
+        # Some values are always 0. The std_dev is 0 then.
+        # This line is used not to divide by 0
+        std = np.array([1 if val == 0 else val for val in std])
+        self.inputs = (self.inputs - mean) / std
+
+    def __len__(self):
+        return self.inputs.shape[0]
+
+    def __getitem__(self, idx):
+        return torch.tensor(self.inputs[idx], dtype=torch.float), torch.tensor(self.outputs[idx], dtype=torch.float)
+
+
+class ResponseTimeDatasetTest(Dataset):
+    """Response Time dataset."""
+
+    def __init__(self):
+        super(ResponseTimeDatasetTest, self).__init__()
+
+        # load the np arrays
+        x_train = pickle.load(open('data/x_train.pickle', 'rb'))
+        x_test = pickle.load(open('data/x_test.pickle', 'rb'))
+
+        x = np.concatenate((x_train, x_test))
+
+        # take the right slice
+        self.inputs = x_test
+
+        # mean reduce x
+        mean = x.mean(axis=0)
+        std = np.std(x, axis=0)
+        std = np.array([1 if val == 0 else val for val in std])
+        self.inputs = (self.inputs - mean) / std
+
+    def __len__(self):
+        return self.inputs.shape[0]
+
+    def __getitem__(self, idx):
+        return torch.tensor(self.inputs[idx], dtype=torch.float)
+
+
+class Net(nn.Module):
+    """Neural network"""
+
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.fc1 = nn.Linear(1964, 64)
+        self.dropout1 = nn.Dropout(p=0.2)
+        self.fc2 = nn.Linear(64, 3)
+
+    def forward(self, x):
+        # Fully connected layer
+        x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        return x
+
+
+def train(net, train_loader, validation_loader, optimizer, scheduler, criterion):
+    """ Train the model """
+    for epoch in range(150):  # loop over the dataset multiple times
+        logging.debug("starting epoch %d" % epoch)
+        train_losses = []
+
+        for cpu_x, cpu_y in train_loader:
+            logging.debug("data loaded")
+
+            net.train()
+            logging.debug("network in train mode")
+
+            # zero the optimizer gradient
+            optimizer.zero_grad()
+            logging.debug("grad zeroed")
+
+            # send inputs and output on gpu
+            gpu_x, gpu_y = cpu_x.to(device), cpu_y.to(device)
+            logging.debug("x and y sent on gpu")
+
+            # compute output
+            gpu_y_pred = net(gpu_x)
+            logging.debug("output computed")
+
+            # compute loss
+            loss = criterion(gpu_y_pred, gpu_y)
+            logging.debug("loss computed")
+
+            # backpropagate
+            loss.backward(retain_graph=True)
+            logging.debug("loss backwarded")
+
+            # step optimizer
+            optimizer.step()
+            logging.debug("optimizer steped")
+
+            # store loss
+            train_losses.append(loss.item())
+
+        # log some metrics about validation_set
+        mean_train_loss = np.mean(train_losses)
+        mean_test_loss = loss_on_testset(net, validation_loader, criterion)
+        mean_test_score = score_on_testset(net, validation_loader)
+        logging.info(
+            "Epoch %d   train loss : %.2f  test loss : %.2f  test score : %.2f"
+            % (epoch, mean_train_loss, mean_test_loss, mean_test_score))
+
+        # save the model
+        torch.save(net.state_dict(), f=PATH)
+        logging.debug("model saved")
+
+        # step the scheduler
+        scheduler.step()
+
+    logging.info('Finished training')
+
+
+if __name__ == '__main__':
+
+    # Data set and data loader
+    train_set = ResponseTimeDataset(train=True)
+    train_loader = DataLoader(train_set, batch_size=2048,
+                              shuffle=True, num_workers=8)
+
+    validation_set = ResponseTimeDataset(train=False)
+    validation_loader = DataLoader(validation_set, batch_size=2048,
+                                   shuffle=True, num_workers=8)
+
+    # neural network
+    net = Net()
+    net.to(device)
+    logging.info('Neural network : {}'.format(str(net)))
+
+    # optimizer
+    optimizer = optim.SGD(net.parameters(), lr=0.01,
+                          momentum=0.9, weight_decay=10e-4)
+
+    # Scheduler : for learning rate decay
+    scheduler = StepLR(optimizer, step_size=40, gamma=0.1)
+
+    # criterion to compute loss
+    criterion = nn.L1Loss()
+
+    # training
+    train(net, train_loader, validation_loader,
+          optimizer, scheduler, criterion)
+
+    # to load an existing model and create the submission
+    net.load_state_dict(torch.load(PATH))
+
+    test_set = ResponseTimeDatasetTest()
+    test_loader = DataLoader(test_set, batch_size=1)
+
+    with open('./data/submission.csv', 'w') as write_file:
+        with open('./data/x_test.csv', 'r') as csvfile:
+
+            # write the first line
+            csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            write_file.writelines(
+                ('emergency vehicle selection,delta selection-departure,delta\
+                    departure-presentation,delta selection-presentation\n'))
+            next(csv_reader)
+
+            # iterate over test data
+            for cpu_x in test_loader:
+                net.eval()
+                gpu_x = cpu_x.to(device)
+                gpu_y_pred = net(gpu_x)
+                numpy_y_pred = gpu_y_pred.cpu().detach().numpy()[0]
+                id_inter = next(csv_reader)[0]
+
+                line = ','.join([str(val) for val in numpy_y_pred])
+                line = id_inter + ',' + line + '\n'
+                write_file.write(line)
---
+++ read_csv.py
@@ -0,0 +1,283 @@
+#coding:utf-8
+
+"""
+csv_data :
+id      name                            format
+# 0     ID row                          101 696 (UPK)
+# 1     ID intervention                 99 334 different ids
+# 2     alert reason category           9 different ids
+# 3     alert reason                    113 different ids
+# 4     on public roads                 [0, 1]
+# 5     floor                           [-6:40] approxiamtely
+# 6     location type                   196 different ids
+# 7     longitude                       .6f 
+# 8     latitude                        .6f
+# 9     identifier of vehicule          612 different ids
+# 10    vehicule type                   35 different names
+# 11    rescue center of the vehicule   76 different ids
+# 12    selection time                  YYYY-MM-DD HH:MM:SS.fff
+# 13    selection time                  YYYYMMDD
+# 14    selection time                  HHMMSS
+# 15    status before the selection     ['Rentré', 'Disponible']
+# 16    nb of sec since last status     int
+# 17    vehicle departed from its RC    [0, 1]
+# 18    longitude before departure      .6f
+# 19    latitude before departure       .6f
+
+brut_data :
+id      name                            type    description
+# 0     ID row                          int     UPK
+# 1     ID intervention                 int     99 334 different ids
+# 2     alert reason category           int     9 different ids
+# 3     alert reason                    int     130 different ids
+# 4     on public roads                 bool    [0, 1]
+# 5     floor                           int     [-6:40] approxiamtely
+# 6     location type                   int     214 different ids
+# 7     longitude                       float   .6f 
+# 8     latitude                        float   .6f
+# 9     identifier of vehicule          int     785 different ids
+# 10    vehicule type                   str     77 different names
+# 11    rescue center of the vehicule   int     94 different ids
+# 12    selection time                  datetime    YYYY-MM-DD HH:MM:SS.fff
+# 13    status before the selection     str     ['Rentré', 'Disponible']
+# 14    nb of sec since last status     int     int
+# 15    vehicle departed from its RC    bool    [0, 1]
+# 16    longitude before departure      float   .6f
+# 17    latitude before departure       float   .6f
+
+dataset :
+id from :to     lenght  meaning
+# 0     :9      9       reason category
+# 9     :122    130     reason
+# 122   :123    1       on public road
+# 123   :124    1       floor
+# 124   :320    214     location type
+# 320   :322    2       lat, lon intervention
+# 322   :934    785     vehicule id
+# 934   :969    77      vehicule name
+# 969   :1045   94      rescue center
+# 1045  :1052   7       year, month, day, hour, minute, second, weekday
+# 1052  :1053   1       if "Rentré"
+# 1053  :1054   1       nb sec since last status
+# 1054  :1055   1       if departed from its RC
+# 1055  :1057   2       latitude, longitude departure
+
+"""
+
+import csv
+import datetime
+import numpy as np
+from dateutil import parser
+import pickle
+import itertools
+
+
+def id_to_one(id, lenght):
+    """(3, 7) gives [0, 0, 0, 1, 0, 0, 0]"""
+    x = np.zeros(lenght)
+    x[id] = 1
+    return x
+
+
+def gps_coord(csv_row):
+    r = np.zeros((2*127))
+    try:
+        gps_track = ','.join(csv_row[21:-2])
+        str_coord = [doublet.split(',') for doublet in gps_track.split('"')[1].split(';')]
+        coord = np.array([[float(l) for l in latlon] for latlon in str_coord])
+        flattened_coord = coord.flatten()
+        for i, coord in enumerate(flattened_coord):
+            r[i]=coord
+    except ValueError:
+        pass
+    return r
+
+
+def gps_time(csv_row):
+    r = np.zeros((3*127))
+    try:
+        gps_track = ','.join(csv_row[21:-2])
+        a=gps_track.split('"')[2].split(';')
+        a = [parser.parse(t.replace(',','')) for t in a]
+
+        for i, value in enumerate(a):
+            # r[7*i+0]=value.year/1000
+            # r[7*i+1]=value.month/12
+            # r[7*i+2]=value.day/60
+            r[3*i+0]=value.hour/24
+            r[3*i+1]=value.minute/60
+            r[3*i+2]=value.second/60
+            # r[7*i+6]=value.weekday()/7
+    except ValueError:
+        pass
+    return r
+
+# ___________________Convert csv into brut_data_______________________________
+def csv_x_data_to_dict_x_data(csv_reader):
+    x_brut_data = []
+    for i, csv_row in enumerate(csv_reader):
+        # Convert csv_data to brut_data
+        if csv_row[6] == '': # Pas de location type renseigné
+            csv_row[6] = 999
+        if csv_row[5] =='100': # Pas de 100e étage à Paris
+            csv_row[5] =='0'
+
+        
+        list_coord = gps_coord(csv_row)
+        list_times = gps_time(csv_row)
+
+        brut_row = {
+            # useless
+            "emergency vehicle selection":                      int(csv_row[0]),            # 0 : ID row (101696)
+            "intervention":                                     int(csv_row[1]),            # 1 : ID intervention (99334)
+            # boolean
+            "intervention on public roads":                     bool(int(csv_row[4])),      # 4 : on public roads (2)
+            "departed from its rescue center":                  bool(int(csv_row[17])),     # 15: vehicle departed from its rescue center (2)
+            #Continue
+            "longitude intervention":                           float(csv_row[7]),          # 7 : longitude (55579)
+            "latitude intervention":                            float(csv_row[8]),          # 8 : latitude (51461)
+            "longitude before departure":                       float(csv_row[18]),         # 16: longitude before departure (1762)
+            "latitude previous departure":                      float(csv_row[19]),         # 17: latitude before departure (1652)
+            "floor":                                            int(csv_row[5]),            # 5 : floor (41)
+            "delta status preceding selection-selection":       int(csv_row[16]),           # 14: number of seconds before the vehicle was selected when its previous status (31769)
+            "delta position gps previous departure-departure" : float(csv_row[20] if csv_row[20] != '' else 0),
+            "OSRM estimated distance":                          float(csv_row[-2]),    
+            "OSRM estimated duration":                          float(csv_row[-1]),
+            # other
+            "selection time":                                   parser.parse(csv_row[12]),  # 12: selection time (101690)
+            "list coord":                                       list_coord,
+            "list times":                                       list_times,
+            # categories
+            "alert reason category":                            int(csv_row[2]),            # 2 : alert reason category (9)
+            "alert reason":                                     int(csv_row[3]),            # 3 : alert reason (113)
+            "location of the event":                            int(float(csv_row[6])),     # 6 : location type (196)
+            "emergency vehicle":                                int(csv_row[9]),            # 9 : identifier of vehicule (612)
+            "rescue center":                                    int(csv_row[11]),           # 11: rescue center of the vehicule (76)
+            "emergency vehicle type":                           csv_row[10],                # 10: vehicule type (35)
+            "status preceding selection":                       csv_row[15],                # 13: status before the selection (2)
+        }
+
+        x_brut_data.append(brut_row)
+
+        if i % 100000 == 99999:
+            print(i+1, "rows converted into x_brut_data")
+
+    return x_brut_data
+
+
+def csv_y_data_to_y_brut_data(csv_reader):
+    y_brut_data = []
+    for i, csv_row in enumerate(csv_reader):
+        brut_row = [
+            #int(csv_row[0]),            # 0 : emergency vehicle selection
+            int(csv_row[1]),            # 1 : delta selection-departure
+            int(csv_row[2]),            # 2 : delta departure-presentation
+            int(csv_row[3]),            # 3 : delta selection-presentation
+            ]
+
+        y_brut_data.append(brut_row)
+
+        if i % 100000 == 99999:
+            print(i+1, "rows converted into brut_data")
+    return y_brut_data
+
+
+if __name__ == '__main__':
+
+    # ------------------Determination of categories---------------------
+    categories = {
+        "alert reason category":      set(),
+        "alert reason":               set(),
+        "location of the event":      set(),
+        "emergency vehicle":          set(),
+        "rescue center":              set(),
+        "emergency vehicle type":     set(),
+        "status preceding selection": set()
+        }
+    
+    for segment in ['train', 'test']:
+        with open('data/x_{}.csv'.format(segment), 'r') as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            next(csv_reader)
+            x_dict_data = csv_x_data_to_dict_x_data(csv_reader)
+
+   
+        for dict_data in x_dict_data:
+            for category, category_set in categories.items():
+                category_set.add(dict_data[category])
+
+    list_categories = {key: list(val) for key, val in categories.items()}
+
+    # ------------------------------------------------------
+
+    list_no_category = [
+        # bollean
+        "intervention on public roads",
+        "departed from its rescue center",
+        # continue
+        "longitude intervention",
+        "latitude intervention",
+        "longitude before departure",
+        "latitude previous departure",
+        "floor",
+        "delta status preceding selection-selection",
+        "delta position gps previous departure-departure",
+        "OSRM estimated distance",
+        "OSRM estimated duration"]
+
+    for segment in ['train', 'test']:
+        with open('data/x_{}.csv'.format(segment), 'r') as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            next(csv_reader)
+            x_dict_data = csv_x_data_to_dict_x_data(csv_reader)
+
+        dataset = []
+        for i_row, dict_row in enumerate(x_dict_data):
+            dataset_row = []
+            
+            for key, value in dict_row.items():
+                # Pour les autres
+                if key in list_no_category:
+                    dataset_row.append(np.array([float(value)]))
+                
+                elif key == 'selection time':
+                    dataset_row.append(np.array([
+                        value.year,
+                        value.month,
+                        value.day,
+                        value.hour,
+                        value.minute,
+                        value.second,
+                        value.weekday()
+                        ]))
+
+                elif key in ['list coord', 'list times']:
+                    dataset_row.append(value)
+
+                # Pour les catagories
+                elif key in list_categories.keys():
+                    # Si la valeur est catégorisée
+                    dataset_row.append(id_to_one(
+                        list_categories[key].index(value),
+                        len(list_categories[key])
+                    ))
+
+            dataset.append(np.concatenate(dataset_row))
+        
+        print('trying to dump data/x_{}.pickle'.format(segment))
+        pickle.dump(np.array(dataset).astype(np.float32), open('data/x_{}.pickle'.format(segment), 'wb'))
+        print('success')
+
+
+# ------------------------------------Y---------------------------------
+
+    with open('data/y_train.csv', 'r') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        next(csv_reader)
+        y_brut_data = csv_y_data_to_y_brut_data(csv_reader)
+
+    dataset = np.array(y_brut_data)
+
+    print('trying to dump data/y_train.pickle')
+    pickle.dump(dataset,open('data/y_train.pickle', 'wb'))
+    print('success')
